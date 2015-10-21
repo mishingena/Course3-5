c45602af4556538c4f693c5683aafdff3d40d1e6
Gena <mishingena@gmail.com>
2015-10-21 19:56:00 +0300

    modify script

--- Scripts/update_version.sh
+++ Scripts/update_version.sh
@@ -1,11 +1,20 @@
 #!/bin/tcsh
+#set INFOPLIST = "${TARGET_BUILD_DIR}/${INFOPLIST_PATH}"
+#set
+#set
+#CFBundleShortVersionString
+set version = `/usr/libexec/PlistBuddy -c "Print :CFBundleShortVersionString" ./Lesson5/Info.plist`
+
+
+
+
 set count = `/usr/libexec/PlistBuddy -c "Print PreferenceSpecifiers:" $1 | grep "Dict" | wc -l`
 set cm1 = `expr $count - 1`
 foreach i (`seq 0 $cm1`)
     set key = `/usr/libexec/PlistBuddy -c "Print PreferenceSpecifiers:${i}:Key" $1`
     if ($key == "app_version") then
         set upd = $2-$3
-        /usr/libexec/PlistBuddy -c "Set PreferenceSpecifiers:${i}:DefaultValue $upd" $1
+        /usr/libexec/PlistBuddy -c "Set PreferenceSpecifiers:${i}:DefaultValue $version" $1
         set version = `/usr/libexec/PlistBuddy -c "Print PreferenceSpecifiers:${i}:DefaultValue" $1`
         echo 'Set version '$version
     endif
